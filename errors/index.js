be57dbb58ddd0defd5403aaedf7cceddaf0e5178
Mohammed Usama <usamabhakrani@gmail.com>
2024-02-29 19:36:01 +0500

    Error Classes Defined

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+const AuthError = require("./auth-error");
+const CustomAPIError = require("./custom-error");
+const BadRequestError = require("./bad-request");
+
+module.exports = { AuthError, CustomAPIError, BadRequestError };
