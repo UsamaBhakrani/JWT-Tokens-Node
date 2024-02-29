3b900450195cfcb8ac21f1c0f3683ef7f1c301d4
Mohammed Usama <usamabhakrani@gmail.com>
2024-02-29 16:20:10 +0500

    JWT Web tokens

---
+++ controllers/main.js
@@ -0,0 +1,16 @@
+const CustomAPIError = require("../errors/custom-error");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+  res.send("Fake Login Route");
+};
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({ msg: `Usama`, secret: luckyNumber });
+};
+
+module.exports = { login, dashboard };
